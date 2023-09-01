6133e346d92378d77e9b1a6ad555b08f08004686
Hendrik Neumann <henne@armselig.net>
2023-09-01 14:01:36 +0000

    feat(the-footer): add component

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-footer
+    reveal
+    class="q-pa-md"
+  >
+    <div class="text-center">
+      Made with
+      <q-icon
+        name="eva-heart"
+        color="red"
+      />
+    </div>
+  </q-footer>
+</template>
--- src/layouts/LayoutBase.vue
+++ src/layouts/LayoutBase.vue
@@ -1,12 +1,14 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-   <the-header />
+    <the-header />
     <q-page-container>
       <router-view />
     </q-page-container>
+    <the-footer />
   </q-layout>
 </template>
 
 <script setup>
 import TheHeader from "@/components/TheHeader.vue";
+import TheFooter from "@/components/TheFooter.vue";
 </script>
